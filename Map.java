f84d5a353bea7e9a76890d21bdd3b495d40250e7
mcarbonellmatos <mcarbonellmatos@smith.edu>
2025-04-17 20:45:13 -0400

    Class update

---
+++ Map.java
@@ -0,0 +1,14 @@
+public class Map {
+    private String peppermintForest;
+    private String nanasNuttHouse;
+    private String licoriceLagoon;
+    private String lollipopPalace;
+    private String frostedPalace;
+    private String queenCandysCastle;
+
+
+    public Map(){
+        
+    }
+}
+
