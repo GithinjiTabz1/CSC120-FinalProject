123c1d6f428765ec4e241fe094458d4d8145b129
GithinjiTabz1 <tgithinji@smith.edu>
2025-04-17 20:21:40 -0400

    updated Castle draft code

--- Castle.java
+++ Castle.java
@@ -1,19 +1,56 @@
 public class Castle {
     protected String name;
     protected boolean isAccessible;
+    protected boolean isInTheCastle;
 
     public Castle(String name) {
         this.name = name;
         this.isAccessible = true;
-        boolean isinthecastle;
+        this.isInTheCastle = false;
     }
 
-
-    // allow resident
-
-
+    public String getName() {
+        return name;
+    }
 
     public boolean isAccessible() {
         return isAccessible;
+    }
+
+    public void setAccessible(boolean accessible) {
+        this.isAccessible = accessible;
+    }
+
+    public boolean isInTheCastle() {
+        return isInTheCastle;
+    }
+
+    // Aliases for entering and exiting the castle
+    public void enter() {
+        goUp();
+    }
+
+    public void exit() {
+        goDown();
+    }
+
+    public void goUp() {
+        if (isAccessible && !isInTheCastle) {
+            isInTheCastle = true;
+            System.out.println("You have entered the " + name + ".");
+        } else if (isInTheCastle) {
+            System.out.println("You are already in the " + name + ".");
+        } else {
+            System.out.println("The " + name + " is not accessible right now.");
+        }
+    }
+
+    public void goDown() {
+        if (isInTheCastle) {
+            isInTheCastle = false;
+            System.out.println("You have exited the " + name + ".");
+        } else {
+            System.out.println("You are not in the " + name + " to exit.");
+        }
     }
 }
