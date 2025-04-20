9e4bc1c590590cd871e3d571a926a88a44232b3c
mcarbonellmatos <mcarbonellmatos@smith.edu>
2025-04-20 17:15:03 -0400

    Constructor and accesors

--- LicoriceLagoon.java
+++ LicoriceLagoon.java
@@ -1,9 +1,30 @@
 public class LicoriceLagoon {
     private int selectNumber;
     private Boolean trivia;
+    private String triviaQuestions;
     private String acceptance;
 
     public LicoriceLagoon(){
-        
+
+
     }
+
+    public int guessNumber(){
+        return this.selectNumber;
+    }
+
+    public Boolean proceedTrivia(){
+        return this.trivia;
+    }
+
+    public String triviaQuestions(){
+        return this.triviaQuestions;
+    }
+
+    public String proceedOrNot(){
+        return this.acceptance;
+
+    }
+
+    /*Navigate through lagoon */
 }
--- Map.java
+++ Map.java
@@ -8,7 +8,33 @@ public class Map {
 
 
     public Map(){
-        
+
     }
+
+    public String peppermintForest(){
+        return this.peppermintForest;
+    }
+
+    public String nanasNutHouse(){
+        return this.nanasNuttHouse;
+    }
+
+    public String licoriceLagoon(){
+        return this.licoriceLagoon;
+    }
+
+    public String lollipopPalace(){
+        return this.lollipopPalace;
+    }
+
+    public String frostedPalace(){
+        return this.frostedPalace;
+    }
+
+    public String queenCandysCastle(){
+        return this.queenCandysCastle;
+    }
+
+    /*Navigate through lagoon */
 }
 
