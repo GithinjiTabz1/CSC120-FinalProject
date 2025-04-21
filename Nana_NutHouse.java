f8f1aed34db4f98777cbcd868bd7e9c94c9f71df
merodriguez27 <merodriguez@smith.edu>
2025-04-21 14:59:36 -0400

    comittingsomething

---
+++ Nana_NutHouse.java
@@ -0,0 +1,47 @@
+import java.util.Scanner;
+import java.util.Random;
+
+public class Nana_NutHouse {
+    private int selectNumber;
+    private boolean discoverIngredients;
+    private int nSugarCubes;
+    private int nCandyFlowers;
+    private int nGlutenFreeFlourPower;
+    
+    public Nana_NutHouse() {
+
+    }
+
+    public int chocoBridgeNumber() {
+        if (//number is odd, bridge breaks, and lose one life) {
+        //if statement is true
+        } else {
+            //false
+        }
+        return this.selectNumber;
+        //how do I make it so the chocolate bridge breaks if an odd number is typed ?
+        //advance to nana nut house if even number is typed? if, else
+    }
+
+
+    public boolean scavengerHunt() {
+        return this.discoverIngredients;
+        //hide sugar, candy, and gluten-free flour for Nana's home. if found advance straight to Nana's house
+    }
+
+    public int findSugarCubes() {
+        //if land on certain color, sugar cube found
+        //how do I create a baseline sugar cube amount
+    }
+
+
+    //scanner class needed 
+    //or use random number generator within a range 
+    //hide a peppermint candy on trail and if found, advance to peppermint forest
+    // how many gingerbreads in the bag, then you advance
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        Random random = new Random();
+    }
+}
---
+++ PathPlaces.java
@@ -0,0 +1,20 @@
+
+public class PathPlaces {
+    
+    protected String name;
+    protected int colorBlocks;
+    protected int selectNumber;
+    protected int secretObjects;
+
+public PathPlaces() {
+    this.name = name;
+    this.colorBlocks = colorBlocks;
+    this.selectNumber = selectNumber;
+    this.secretObjects = secretObjects;
+}
+
+// attributes
+// constructor 
+//random number
+//random objects
+}
