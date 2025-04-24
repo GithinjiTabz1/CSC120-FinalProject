a6da189b55ffc052c09ea68826ae2e8bc8d23107
mcarbonellmatos <mcarbonellmatos@smith.edu>
2025-04-24 11:00:01 -0400

    Map Progress

--- Map.java
+++ Map.java
@@ -1,24 +1,25 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class Map {
-    private Boolean position ;
-    private String colorBlocks;
-    private String playerPosition;
+    private final String[] colorBlocks = {"Red", "Purple", "Yellow", "Blue", "Orange", "Green"};
+    private List<String> tiles;
 
 
-    /* Constructor */
-    public Map(){
-
+    /* Constructor
+     * Array List which contains each color (colorBlocks).
+     * Creation of loop which will run # of tile times
+     * Add color to the tiles using the colorBlocks array
+     */
+    public Map(int numberOfTiles){
+        Map = new ArrayList<>();
+        for (int i = 0; i <numberOfTiles; i++){
+            Map.add(colorBlocks[i % colorBlocks.length]);
+        }
     }
 
-    public Boolean indexPosition(){
-        return this.position;
-    }
-
-    public String gameBlocks(){
-        return this.colorBlocks;
-    }
-
-    public String playerPosition(){
-        return this.playerPosition;
+    public void printMap(Player player){
+        for (int i)
     }
 
 }
