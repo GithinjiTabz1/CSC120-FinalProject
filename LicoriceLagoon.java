dc47d1a29680e1ef93d115dd596c01869339f10a
merodriguez27 <merodriguez@smith.edu>
2025-04-29 13:15:33 -0400

    comitting

--- LicoriceLagoon.java
+++ LicoriceLagoon.java
@@ -1,43 +1,6 @@
 import java.util.Scanner;
 
-
-    }
-
-    public int guessNumber(){
-        return this.selectNumber;
-        /*
-         * When you land in the lagoon you will need to guess a number from 1 to 10.
-         * If you guess it incorectly, you will get a random trivia question.
-         * Array List of the trivia questions.
-         */
-    }
-
-    public Boolean proceedTrivia(){
-        /* 
-         * If number guessed is incorrect proceed to trivia questions.
-         */
-        return this.trivia;
-    }
-
-    public String triviaQuestions(){
-        /*
-         * Random questions in an Array List
-         */
-        questions = new ArrayList<>();
-
-        return this.triviaQuestions;
-    }
-
-    public String proceedOrNot(){
-        /*
-         * If trivia questions wrongly answered then player loses
-           and therefore proceeds through the long way and does not get directly
-           relocated to were the catsles are.
-         * If correctly answered then player gets relocated to the first castle.
-         */
-        return this.acceptance;
-        
-    public class LicoriceLagoon extends PathPlaces{
+public class LicoriceLagoon extends PathPlaces{
 
     public LicoriceLagoon(String name, int playerInput) {
         super (name, playerInput);
@@ -69,10 +32,13 @@ import java.util.Scanner;
             }
             
     }
+    public void finalSentiment() {
+        System.out.println("That's right! You have completed the challenge and may progress to the next tile.");
+        }
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        LicoriceLagoon llTest = new LicoriceLagoon("Licorice Lagoon", 2003, PathPlaces.triviaQuestions);
+        LicoriceLagoon llTest = new LicoriceLagoon("Licorice Lagoon", 2003);
         llTest.printPathName();
         }
     }
