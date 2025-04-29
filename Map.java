01848f1afde3e7a51a7e292c81b2f0cf7ddada36
mcarbonellmatos <mcarbonellmatos@smith.edu>
2025-04-29 11:35:39 -0400

    map update

--- Map.java
+++ Map.java
@@ -11,19 +11,31 @@ public class Map {
         this.colorBlocks = colorBlocks;
         this.playerPosition = playerPosition;
     }
- 
+
+    /*
+     * Index of each block of the map.
+     */
     public Boolean indexPosition(){
         return this.position;
     }
  
+    /*
+     * Tiles of the map.
+     */
     public String gameBlocks(){
         return this.colorBlocks;
     }
  
+    /*
+     * Name of the tile (color and index) which the player is in.
+     */
     public String playerPosition(){
         return this.playerPosition;
     }
 
+    /*
+     * Establishing the player position, color blocks and index position of the tiles in the map.
+     */
     public void setPlayerPosition(String playerPosition) {
         this.playerPosition = playerPosition;
     }
@@ -37,23 +49,41 @@ public class Map {
     }
 
     public static void main(String[] args){
+        /*
+         * Map array list.
+         */
         ArrayList<Map> map = new ArrayList<>();
         FullSpinner spinner = new FullSpinner();
         spinner.addParts();
+        /*
+         * Setting the colors of the tiles.
+         */
         String[] colors = {"Red", "Purple", "Yellow", "Blue", "Orange", "Green"};
 
-        for (int i = 0; i < 60; i++){
+        /*
+         * Loop for the map and tiles.
+         */
+        for (int i = 0; i < 61; i++){
             String color = colors[i % colors.length];
             Map tiles = new Map(false, color, "None");
             map.add(tiles);
         }
 
+        /*
+         * Position and index gets set in the map.  
+         */
         map.get(playerIndex).setIndexPosition(true);
         map.get(playerIndex).setPlayerPosition(Player.getName);
 
-        String spunColor = spinner.spun();
+        /*
+         * using the full spinner and spinner parts the color that was randomly selected gets printed.
+         */
+        String spunColor = spinner.spunColor();
         System.out.println("Spinner result: " + spunColor);
 
+        /*
+         * Array to make the player move to repsective tile(color) that was spun in the spinner.
+         */
         int currentPos = Player.getPosition();
         int newIndex = -1;
         for (int i = playerIndex + 1; i < map.size(); i++){
@@ -64,7 +94,9 @@ public class Map {
             else {
                 System.out.println("There are no tiles of that color ahead.");
             }
-
+        /*
+         * 
+         */
         if (newIndex != -1){
             map.get(playerIndex).setIndexPosition(false);
             map.get(playerIndex).setPlayerPosition("None");
@@ -72,6 +104,6 @@ public class Map {
             map.get(newIndex).setPlayerPosition(Player.getName);
             playerIndex = newIndex;
             System.out.println("Player " + getName + " is now at " + playerIndex);
-        }  
+        }
     }
 }
