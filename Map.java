d6e307f7061434c569ca71674598f12b342c4b8a
GithinjiTabz1 <tgithinji@smith.edu>
2025-05-09 08:38:08 -0400

    added javadoc comments

--- Map.java
+++ Map.java
@@ -1,59 +1,95 @@
-
+/**
+ * Represents a tile on the game board in Candy Land.
+ * Each tile has a position status, color, and may contain a special location.
+ * Tiles are used to track player movement and special locations on the board.
+ */
 public class Map {
-    private Boolean position ;
+    private Boolean position;
     private String colorBlocks;
     private String playerPosition;
 
-
-    /* Constructor */
-    public Map(Boolean position, String colorBlocks, String playerPosition){
+    /**
+     * Constructs a new Map tile with specified position, color, and player position.
+     * 
+     * @param position indicates if this is a valid position on the board
+     * @param colorBlocks the color of this tile
+     * @param playerPosition the name of the special location if any, or "None"
+     */
+    public Map(Boolean position, String colorBlocks, String playerPosition) {
         this.position = position;
         this.colorBlocks = colorBlocks;
         this.playerPosition = playerPosition;
     }
 
-
-    /*
-     * Index of each block of the map.
+    /**
+     * Gets the position status of this tile.
+     * 
+     * @return true if this is a valid position on the board
      */
-    public Boolean indexPosition(){
+    public Boolean indexPosition() {
         return this.position;
     }
- 
-    /*
-     * Tiles of the map.
-     */
 
-    public String gameBlocks(){
+    /**
+     * Gets the color of this tile.
+     * 
+     * @return the color of this tile
+     */
+    public String gameBlocks() {
         return this.colorBlocks;
     }
- 
-    /*
-     * Name of the tile (color and index) which the player is in.
-     */
 
-    public String getPlayerPosition(){
+    /**
+     * Gets the name of the special location on this tile.
+     * 
+     * @return the name of the special location or "None" if no special location
+     */
+    public String getPlayerPosition() {
         return this.playerPosition;
     }
 
+    /**
+     * Sets the name of the special location on this tile.
+     * 
+     * @param playerPosition the name of the special location
+     */
     public void setPlayerPosition(String playerPosition) {
         this.playerPosition = playerPosition;
     }
 
+    /**
+     * Sets the color of this tile.
+     * 
+     * @param colorBlocks the new color for this tile
+     */
     public void setColorBlocks(String colorBlocks) {
         this.colorBlocks = colorBlocks;
     }
 
+    /**
+     * Sets the position status of this tile.
+     * 
+     * @param position the new position status
+     */
     public void setIndexPosition(Boolean position) {
         this.position = position;
     }
 
+    /**
+     * Gets the color of this tile.
+     * 
+     * @return the color of this tile
+     */
     public String getColor() {
         return this.colorBlocks;
     }
+
+    /**
+     * Gets the name of the special location on this tile.
+     * 
+     * @return the name of the special location or "None" if no special location
+     */
     public String getLocation() {
         return this.playerPosition;
     }
-
 }
-
